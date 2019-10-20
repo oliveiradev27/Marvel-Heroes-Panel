d3d1b54bd82fb7818101150c70eaea95f4bb5706
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-10-20 16:51:09 -0300

    Commit para adicionar arquivos faltantes no anterior

---
+++ app/src/main/java/br/espartano/marvelheroescatalog/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.espartano.marvelheroescatalog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
