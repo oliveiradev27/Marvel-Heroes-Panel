af6a0e9a44f3d1c31f8e1ef934c139a7f651c007
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-10-20 22:48:25 -0300

    Altera cores do tema

---
+++ app/src/main/java/br/espartano/marvelheroescatalog/paging/CharactersDataSource.kt
@@ -0,0 +1,9 @@
+package br.espartano.marvelheroescatalog.paging
+
+import br.espartano.marvelheroescatalog.services.MarvelApi
+import io.reactivex.disposables.CompositeDisposable
+
+class CharactersDataSource(
+    private val marvelApi : MarvelApi,
+    private val compositeDisposable: CompositeDisposable
+)
