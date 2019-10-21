a59e86d112332c14296d7e84e534d881f7796049
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-10-20 22:49:27 -0300

    Adiciona a máquina de estado que fará a comunicação entre activity e viewModel

---
+++ app/src/main/java/br/espartano/marvelheroescatalog/viewmodels/states/CharactersStates.kt
@@ -0,0 +1,11 @@
+package br.espartano.marvelheroescatalog.viewmodels.states
+
+import br.espartano.marvelheroescatalog.data.api.Character
+
+sealed class CharactersStates {
+
+    object InitialState : CharactersStates()
+    object Loading : CharactersStates()
+    class Loaded(val caracters : List<Character>) : CharactersStates()
+    data class Error(val message : String) : CharactersStates()
+}
