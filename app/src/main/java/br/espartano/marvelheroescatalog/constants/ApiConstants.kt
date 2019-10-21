82806d1d32894ecb35f74e95ab4751f4439a95a8
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-10-20 22:47:20 -0300

    Adiciona constantes que serão usadas nas requisições

---
+++ app/src/main/java/br/espartano/marvelheroescatalog/constants/ApiConstants.kt
@@ -0,0 +1,10 @@
+package br.espartano.marvelheroescatalog.constants
+
+object ApiConstants {
+    const val API_KEY = "1c2cfd02d983528a5a0b8d1bcd6625e1"
+    const val PRIVATE_API_KEY = "06f18eb89a7a10046de78bdac7c7976d07473b73"
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
+    const val HEADER_HASH = "hash"
+    const val HEADER_TS = "ts"
+    const val HEADER_API_KEY = "apikey"
+}
