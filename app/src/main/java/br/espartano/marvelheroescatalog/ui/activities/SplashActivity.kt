59ae583540a692eccc4f8cef756f3d309dbcde0c
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-10-20 23:06:04 -0300

    Adiciona tela de Splash

---
+++ app/src/main/java/br/espartano/marvelheroescatalog/ui/activities/SplashActivity.kt
@@ -0,0 +1,21 @@
+package br.espartano.marvelheroescatalog.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import br.espartano.marvelheroescatalog.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({ showListActivity() }, 2000)
+    }
+
+    private fun showListActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
