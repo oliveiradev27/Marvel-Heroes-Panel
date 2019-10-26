15e766c9251ab55ffa5b811f54cfd9f0d01da74d
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-10-26 12:59:17 -0300

    Adiciona camada de Use Case

---
+++ app/src/main/java/br/espartano/marvelheroescatalog/usecase/CharactersUseCase.kt
@@ -0,0 +1,12 @@
+package br.espartano.marvelheroescatalog.usecase
+
+import br.espartano.marvelheroescatalog.data.api.Character
+import br.espartano.marvelheroescatalog.repository.CharactersRepository
+import io.reactivex.Observable
+
+class CharactersUseCase (val repository: CharactersRepository) {
+
+    fun getCharacteres(page: Int) : Observable<List<Character>>? {
+        return repository.getCharacters(page)
+    }
+}
