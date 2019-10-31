4ed866cd617dd27ac54f02aacb559bf049729fe3
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-10-30 23:32:30 -0300

    Transfere lógica de paginação para UseCase

--- app/src/main/java/br/espartano/marvelheroescatalog/usecase/CharactersUseCase.kt
+++ app/src/main/java/br/espartano/marvelheroescatalog/usecase/CharactersUseCase.kt
@@ -7,6 +7,6 @@ import io.reactivex.Observable
 class CharactersUseCase (private val repository: CharactersRepository) {
 
     fun getCharacteres(page: Int) : Observable<List<Character>>? {
-        return repository.getCharacters(page)
+        return repository.getCharacters(page * 10)
     }
 }
