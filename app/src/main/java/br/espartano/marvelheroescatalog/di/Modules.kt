4fa510c73d14ebba93698f31079a7af1915684e4
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-10-31 23:17:01 -0300

    Cria arquivo contendo os módulos a serem providos pelo Koin

---
+++ app/src/main/java/br/espartano/marvelheroescatalog/di/Modules.kt
@@ -0,0 +1,15 @@
+package br.espartano.marvelheroescatalog.di
+
+import br.espartano.marvelheroescatalog.repository.CharactersNetworkRepository
+import br.espartano.marvelheroescatalog.repository.CharactersRepository
+import br.espartano.marvelheroescatalog.schedulers.AppSchedulerProvider
+import br.espartano.marvelheroescatalog.usecase.CharactersUseCase
+import br.espartano.marvelheroescatalog.viewmodels.CharactersViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModules = module {
+    single<CharactersRepository>{ CharactersNetworkRepository() }
+    factory { CharactersUseCase(get()) }
+    viewModel { CharactersViewModel(get(), AppSchedulerProvider()) }
+}
