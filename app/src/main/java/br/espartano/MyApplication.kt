16f452dfa9fcbe5ab358a1c57056c20545a08e40
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-10-31 23:17:49 -0300

    Cria Application customizada, para a injeção das dependências em tempo de construção do app

---
+++ app/src/main/java/br/espartano/MyApplication.kt
@@ -0,0 +1,22 @@
+package br.espartano
+
+import android.app.Application
+import br.espartano.marvelheroescatalog.di.appModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidFileProperties
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@MyApplication)
+            androidFileProperties()
+            modules(appModules)
+        }
+    }
+}
