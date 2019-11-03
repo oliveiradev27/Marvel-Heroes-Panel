a66cfc9eb9299ebfe8adca8b8671abd358f6d9a7
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-11-03 15:19:26 -0300

    Transfere detalhes de implementação para classe robot

--- app/src/androidTest/java/br/espartano/marvelheroescatalog/ui/activities/MainActivityRobots.kt
+++ app/src/androidTest/java/br/espartano/marvelheroescatalog/ui/activities/MainActivityRobots.kt
@@ -1,11 +1,23 @@
 package br.espartano.marvelheroescatalog.ui.activities
 
-object MainActivityRobotsConstants {
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import br.espartano.marvelheroescatalog.R
 
+object MainActivityRobotsConstants {
+    const val HERO_NAME = "Espartano"
 }
 
 class MainActivityGiven {
 
+    fun initializeActivity() {
+        val scenario = ActivityScenario.launch(MainActivity::class.java)
+        scenario.moveToState(Lifecycle.State.RESUMED)
+    }
 }
 
 class MainActivityWhen {
@@ -14,4 +26,8 @@ class MainActivityWhen {
 
 class MainActivityThen {
 
+    fun validateTitle(title : String) {
+        onView(withId(R.id.tv_hero_name))
+            .check(matches(withText(title)))
+    }
 }
--- app/src/androidTest/java/br/espartano/marvelheroescatalog/ui/activities/MainActivityTest.kt
+++ app/src/androidTest/java/br/espartano/marvelheroescatalog/ui/activities/MainActivityTest.kt
@@ -1,8 +1,7 @@
 package br.espartano.marvelheroescatalog.ui.activities
 
-import androidx.lifecycle.Lifecycle
-import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import br.espartano.marvelheroescatalog.ui.activities.MainActivityRobotsConstants.HERO_NAME
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -10,9 +9,14 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
+    private fun given(func: MainActivityGiven.() -> Unit) = MainActivityGiven().apply { func() }
+    private fun `when`(func: MainActivityWhen.() -> Unit) = MainActivityWhen().apply { func() }
+    private fun then(func: MainActivityThen.() -> Unit) = MainActivityThen().apply { func() }
+
+
     @Test
     fun whenStartActivity_mustLoadInitialCharacters() {
-       val scenario = ActivityScenario.launch(MainActivity::class.java)
-        scenario.moveToState(Lifecycle.State.CREATED)
+        given { initializeActivity() }
+        then { validateTitle(HERO_NAME) }
     }
 }
