40424e332bddc42b4221b6da34d3d6e0548e29d5
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-11-03 15:02:02 -0300

    Criar arquivo implementação da Characters repository

---
+++ app/src/androidTest/java/br/espartano/marvelheroescatalog/repository/CharactersTestRepository.kt
@@ -0,0 +1,16 @@
+package br.espartano.marvelheroescatalog.repository
+
+import br.espartano.marvelheroescatalog.data.api.Character
+import br.espartano.marvelheroescatalog.data.api.Thumbnail
+import io.reactivex.Observable
+
+class CharactersTestRepository: CharactersRepository {
+
+    override fun getCharacters(page: Int): Observable<List<Character>>? {
+        return Observable.just(
+            listOf(Character(1, "Espartano", "Overpower",
+                Thumbnail("", ""))
+            )
+        )
+    }
+}
