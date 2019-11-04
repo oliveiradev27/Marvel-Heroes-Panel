e23fbd1f3818fadaa19b74993dde21645eed7a46
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-11-03 22:15:10 -0300

    Adiciona o módulo com as dependências com erro, para realizar testes

--- app/src/androidTest/java/br/espartano/marvelheroescatalog/di/Modules.kt
+++ app/src/androidTest/java/br/espartano/marvelheroescatalog/di/Modules.kt
@@ -1,15 +1,24 @@
 package br.espartano.marvelheroescatalog.di
 
+import br.espartano.marvelheroescatalog.repository.CharactersErrorTestRepository
 import br.espartano.marvelheroescatalog.repository.CharactersRepository
 import br.espartano.marvelheroescatalog.repository.CharactersTestRepository
+import br.espartano.marvelheroescatalog.schedulers.SchedulerProvider
 import br.espartano.marvelheroescatalog.schedulers.TrampolineSchedulerProvider
 import br.espartano.marvelheroescatalog.usecase.CharactersUseCase
 import br.espartano.marvelheroescatalog.viewmodels.CharactersViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-val testModules = module {
+val testModule = module {
     single <CharactersRepository>{ CharactersTestRepository() }
     factory { CharactersUseCase(get()) }
     viewModel { CharactersViewModel(get(), TrampolineSchedulerProvider()) }
+}
+
+val testErrorModule = module {
+    single<CharactersRepository> { CharactersErrorTestRepository() }
+    single<SchedulerProvider> { TrampolineSchedulerProvider() }
+    factory {CharactersUseCase(get()) }
+    viewModel { CharactersViewModel(get(), get()) }
 }
