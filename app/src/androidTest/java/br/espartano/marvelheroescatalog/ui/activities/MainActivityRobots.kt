8b95da1a58cad3c5c434dcfba2832425446fb950
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2019-11-03 22:15:58 -0300

    Adiciona o caso de testes para validar mensagem de erro

--- app/src/androidTest/java/br/espartano/marvelheroescatalog/ui/activities/MainActivityRobots.kt
+++ app/src/androidTest/java/br/espartano/marvelheroescatalog/ui/activities/MainActivityRobots.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import br.espartano.marvelheroescatalog.R
+import br.espartano.marvelheroescatalog.di.testErrorModule
+import br.espartano.marvelheroescatalog.di.testModule
+import org.koin.core.context.loadKoinModules
 
 object MainActivityRobotsConstants {
     const val HERO_NAME = "Espartano"
@@ -17,6 +19,14 @@ class MainActivityGiven {
     fun initializeActivity() {
         val scenario = ActivityScenario.launch(MainActivity::class.java)
         scenario.moveToState(Lifecycle.State.RESUMED)
+    }
+
+    fun initializeSucessDependencies() {
+        loadKoinModules(testModule)
+    }
+
+    fun initializeErrorDependencies() {
+        loadKoinModules(testErrorModule)
     }
 }
 
@@ -29,5 +39,10 @@ class MainActivityThen {
     fun validateTitle(title : String) {
         onView(withId(R.id.tv_hero_name))
             .check(matches(withText(title)))
+    }
+
+    fun validateVisibilityDialogError() {
+        onView(withText(R.string.system_body_error))
+            .check(matches(isDisplayed()))
     }
 }
--- app/src/androidTest/java/br/espartano/marvelheroescatalog/ui/activities/MainActivityTest.kt
+++ app/src/androidTest/java/br/espartano/marvelheroescatalog/ui/activities/MainActivityTest.kt
@@ -1,10 +1,14 @@
 package br.espartano.marvelheroescatalog.ui.activities
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import br.espartano.marvelheroescatalog.di.testErrorModule
+import br.espartano.marvelheroescatalog.di.testModule
 import br.espartano.marvelheroescatalog.ui.activities.MainActivityRobotsConstants.HERO_NAME
+import org.junit.After
 
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.unloadKoinModules
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
@@ -13,10 +17,30 @@ class MainActivityTest {
     private fun `when`(func: MainActivityWhen.() -> Unit) = MainActivityWhen().apply { func() }
     private fun then(func: MainActivityThen.() -> Unit) = MainActivityThen().apply { func() }
 
+    @After
+    fun tearDown() {
+        unloadKoinModules(
+            arrayListOf(
+                testModule,
+                testErrorModule)
+        )
+    }
 
     @Test
     fun whenStartActivity_mustLoadInitialCharacters() {
-        given { initializeActivity() }
+        given {
+            initializeSucessDependencies()
+            initializeActivity()
+        }
         then { validateTitle(HERO_NAME) }
+    }
+
+    @Test
+    fun whenStartActivity_showDialogError() {
+        given {
+            initializeErrorDependencies()
+            initializeActivity()
+        }
+        then { validateVisibilityDialogError() }
     }
 }
