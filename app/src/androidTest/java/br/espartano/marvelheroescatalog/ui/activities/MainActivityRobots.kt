1a57442f870110e6a0a89152a57102ae1488be56
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2021-09-11 23:21:26 -0300

    corrections widcards ktlint

--- app/src/androidTest/java/br/espartano/marvelheroescatalog/ui/activities/MainActivityRobots.kt
+++ app/src/androidTest/java/br/espartano/marvelheroescatalog/ui/activities/MainActivityRobots.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import br.espartano.marvelheroescatalog.R
 import br.espartano.marvelheroescatalog.di.testErrorModule
 import br.espartano.marvelheroescatalog.di.testModule
