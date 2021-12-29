266f24fd17d64ee24edd025b2113870986097ccd
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2021-12-29 11:21:00 -0300

    fix: remove unused imports

--- app/src/androidTest/java/br/espartano/marvelheroescatalog/ui/activities/MainActivityRobots.kt
+++ app/src/androidTest/java/br/espartano/marvelheroescatalog/ui/activities/MainActivityRobots.kt
@@ -8,8 +8,6 @@ import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import br.espartano.marvelheroescatalog.R
-import br.espartano.marvelheroescatalog.repository.CharactersErrorTestRepository
-import br.espartano.marvelheroescatalog.repository.CharactersRepository
 
 object MainActivityRobotsConstants {
     const val HERO_NAME = "Espartano"
