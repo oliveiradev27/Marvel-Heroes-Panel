4a7230b5e3fb34a6c36dbc89bd32cda21654d5ec
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2022-03-27 11:58:10 -0300

    feat: update versions

--- app/src/androidTest/java/br/espartano/marvelheroescatalog/ui/activities/MainActivityTest.kt
+++ app/src/androidTest/java/br/espartano/marvelheroescatalog/ui/activities/MainActivityTest.kt
@@ -1,6 +1,8 @@
 package br.espartano.marvelheroescatalog.ui.activities
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import br.espartano.marvelheroescatalog.data.api.Character
+import br.espartano.marvelheroescatalog.data.api.Thumbnail
 import br.espartano.marvelheroescatalog.repository.CharactersErrorTestRepository
 import br.espartano.marvelheroescatalog.repository.CharactersRepository
 import br.espartano.marvelheroescatalog.repository.CharactersTestRepository
@@ -9,6 +11,9 @@ import br.espartano.marvelheroescatalog.ui.activities.MainActivityRobotsConstant
 import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import io.mockk.every
+import io.mockk.mockk
+import io.reactivex.Observable
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -21,13 +26,21 @@ class MainActivityTest {
     var hiltRule = HiltAndroidRule(this)
 
     @BindValue
-    var charactersTestRepository: CharactersRepository =  CharactersTestRepository()
+    var charactersTestRepository: CharactersRepository =  mockk(relaxed = true)
 
     private fun given(func: MainActivityGiven.() -> Unit) = MainActivityGiven().apply { func() }
     private fun then(func: MainActivityThen.() -> Unit) = MainActivityThen().apply { func() }
 
     @Test
     fun whenStartActivity_mustLoadInitialCharacters() {
+        every { charactersTestRepository.getCharacters(any()) } returns Observable.just(
+            listOf(
+                Character(1, "Espartano", "Overpower",
+                Thumbnail("", "")
+                )
+            )
+        )
+
         given {
             initializeActivity()
         }
